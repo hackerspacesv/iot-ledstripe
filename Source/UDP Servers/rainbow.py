dc8a2077e68ad6d7dec3abe290b526b61f95eff5
Yeffri J. Salazar <yeffrisalazar1@gmail.com>
2016-12-09 01:21:45 -0500

    agregando excepcion de teclado
    Cuando el teclado haga una interrupcion o una excepcion con ctrl +c limpia la tira de leds :)

--- Source/UDP Servers/rainbow.py
+++ Source/UDP Servers/rainbow.py
@@ -1,20 +1,4 @@
-# IoT LED Stripe Controller v0.1
-# UDP Server to display rainbow colors on the stripe
-# Copyright 2015. Mario Gómez <mario _dot_ gomez -at- teubi.co>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License.
-#
-#  This program is distributed in the hope that it will be useful, but 
-#  WITHOUT ANY WARRANTY; without even the implied warranty of 
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
-#  General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License 
-#  along with this program.  If not, see 
-#  <http://www.gnu.org/licenses/>.
-
+  
 import sys, pygame
 import socket, math
 
@@ -34,9 +18,9 @@ gammaG = [None] * 181
 gammaB = [None] * 181
 
 for i in range(0,181):
-  gammaR[i] = int(round(25 * math.pow(i/181.0, 1/0.75)))
-  gammaG[i] = int(round(25 * math.pow(i/181.0, 1/0.50)))
-  gammaB[i] = int(round(25 * math.pow(i/181.0, 1/0.40)))
+  gammaR[i] = int(round(255 * math.pow(i/181.0, 1/0.75)))
+  gammaG[i] = int(round(255 * math.pow(i/181.0, 1/0.50)))
+  gammaB[i] = int(round(255 * math.pow(i/181.0, 1/0.40)))
 
 
 # Pre-Compute rainbow
@@ -86,16 +70,27 @@ for i in range(0,120):
       bVal = 0
 
 step = 0
-while True:
-  for i in range(0,120):
-    rainbowPos = i+step%120
-    if rainbowPos > 119:
-      rainbowPos = rainbowPos-120
-    packet[(i*4)+0] = i
-    packet[(i*4)+1] = gammaR[rainbow[(rainbowPos*3)+0]]
-    packet[(i*4)+2] = gammaG[rainbow[(rainbowPos*3)+1]]
-    packet[(i*4)+3] = gammaB[rainbow[(rainbowPos*3)+2]]
+try:
+  while True:
+      for i in range(0,120):
+        rainbowPos = i+step%120
+        if rainbowPos > 119:
+          rainbowPos = rainbowPos-120
+        packet[(i*4)+0] = i
+        packet[(i*4)+1] = gammaR[rainbow[(rainbowPos*3)+0]]
+        packet[(i*4)+2] = gammaG[rainbow[(rainbowPos*3)+1]]
+        packet[(i*4)+3] = gammaB[rainbow[(rainbowPos*3)+2]]
+      payload = "".join(map(chr, packet))
+      sock.sendto(payload,(UDP_IP, UDP_PORT))
+      pygame.time.wait(25)
+      step = step + 1
+except KeyboardInterrupt:
+  print "adios :) "
+  packet  = [None] * 480 # Full LED Stripe
+  for a in range(120):
+    packet[(a*4)]=a;
+    packet[(a*4)+1]=0
+    packet[(a*4)+2]=0
+    packet[(a*4)+3]=0
   payload = "".join(map(chr, packet))
   sock.sendto(payload,(UDP_IP, UDP_PORT))
-  pygame.time.wait(25)
-  step = step + 1
